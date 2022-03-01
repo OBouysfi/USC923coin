b6bc4d759eff670c53c36352092ad733dcfd8f39
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:38:05 +0000

    Update whitepaper.blade.php

---
+++ resources/views/front/whitepaper.blade.php
@@ -0,0 +1,51 @@
+  <!--servise-->
+  <section class="py-5">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-sm"> 
+                <div class="card bg-dark text-light mb-3">
+                    <div class="card-body text-center">
+                        <img style="margin-bottom: 15px;" class="card-img-top" src="{{asset('front/img/home.svg')}}" width="300" height="200" alt="" srcset="">
+                        <h3 class="card-title">USC923coin</h3>   
+                        <p class="card-text lead">
+                            <span style="color: rgb(211, 179, 1);font-weight: bold;">$USC923coin</span> aims to be the most popular and widely used cryptocurrency in the world.
+                            It is the cryptocurrency of the people.
+                            a movement. 100% of tokens to be monetized on Binance Smart Total 
+                            supply: $100,000,000,000,000 USC923coin.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card bg-dark text-light mb-3">
+                    <div class="card-body text-center">
+                        <img style="margin-bottom: 15px;" class="card-img-top" src="{{asset('front/img/abou.svg')}}" width="300" height="200" alt="" srcset="">
+                        <h3 class="card-title">About Us</h3>
+                        <p class="card-text lead">
+                            A Decentralized USC923 TOKEN that Evolved into a Vibrant Ecosystem
+                            If 2022 taught the crypto world anything,
+                            it's that community-driven tokens are in HIGH demand. 
+                            The PEOPLE want control of their token, 
+                            and strong communities of HODLers can be built that have each other's back.
+                            2022 - Year of the USC923
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="card bg-dark text-light mb-3">
+                    <div class="card-body text-center">
+                        <img style="margin-bottom: 15px;" class="card-img-top" src="{{asset('front/img/money.svg')}}" width="300" height="200" alt="" srcset="">
+                        <h4 class="card-title">money future</h4>
+                        <p class="card-text lead">
+                            USC923 is among the coins that will be the most used, 
+                            and will pioneer this concept in the digital token space. 
+                            Disrupting the traditional financial system and pioneering the digital use of traditional currencies, 
+                            USC923Coin supports enterprise growth and innovation across the corporate space.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
