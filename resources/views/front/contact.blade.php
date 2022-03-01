4ca6e133eac70d10eb9ad38d770103eaeb1db3d3
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:37:50 +0000

    Update contact.blade.php

---
+++ resources/views/front/contact.blade.php
@@ -0,0 +1,36 @@
+<section id="contact" class="py-4">
+    <div class="container">
+        <div class="wrapper rounded d-flex align-items-stretch">
+            <div class="bg-dark text-light">
+                <div class="text-white text-center mt-5"> <i style="font-size: 30px;" class="bi bi-envelope-check-fill"></i> </div>
+                <div style="font-weight: bold;" class="pt-5 cursive m-3 lead"> Please describe your product idea in a nutshell </div>
+                <div style="font-weight: bold;" class="pt-sm-5 pt-5 cursive mt-sm-5 m-3 lead"> We need your email to reach you back </div>
+            </div>
+            <div class="contact-form">
+                <div class="h3">Hire Us</div>
+                <form>
+                    <div class="form-group pt-3"> <label for="message">Message</label> <textarea name="message" placeholder="write your message..." class="form-control" required></textarea> </div>
+                    <div class="d-flex align-items-center flex-wrap justify-content-between pt-4">
+                        <div class="form-group pt-lg-2 pt-3"> <label for="name">Your Name</label> <input type="text" name="name" placeholder="Your Name.." class="form-control" required> </div>
+                        <div class="form-group pt-lg-2 pt-3"> <label for="name">Your Email</label> <input type="email" name="email" placeholder="example@gmail.com" class="form-control" required> </div>
+                    </div>
+                    <div class="d-flex align-items-center flex-wrap justify-content-between pt-lg-5 mt-lg-4 mt-5">
+                        <div style="background-color: rgb(211, 179, 1);color: black;font-weight: bold;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal"> Submit </div>
+                    </div>
+                </form>
+            </div>
+        </div> <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="myModalLabel">Thank You <span class="fas fa-heart"></span> </h5> <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="ps-2">Thank You for sharing your views with us. We will get back to you as soon as possible.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
