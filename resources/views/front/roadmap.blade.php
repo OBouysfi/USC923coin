ad70e9161e15f604b19e15233861c5945e12d5a5
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:38:00 +0000

    Update roadmap.blade.php

---
+++ resources/views/front/roadmap.blade.php
@@ -0,0 +1,48 @@
+<section id="rode" class="py-5">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md">
+                <img src="{{asset('front/img/rodenap.svg')}}" alt="" class="img-fluid">
+            </div>
+            <div class="col-md py-4">
+                <h2>Road Map</h2>
+                <p class="lead">
+                <b style="color: rgb(211, 179, 1);">2022 Q1 =</b> <br>
+                    private testnet Devent 1.0 Devent 1.0 & oasis sdk bug bounty public testnet questnet competition
+                    Amber network upcoming mainnet lanch              
+                <br>
+                <b style="color: rgb(211, 179, 1);">2022 Q2 =</b> <br>
+                    -Birth of $USC923coin , first USC923coin coin built on binance smart chain.
+                    -15% of the max $USC923coin supply airdropped to our community members.
+                    -Website launch.
+                    --Listed on Coingecko
+                <br>
+                <b style="color: rgb(211, 179, 1);">2022 Q3 =</b><br>
+                <b>STARTING THE PROJET =</b><br>
+                    ico concept creation and withpaper drafting, 
+                    sell all king of legal documents including coin legal concept, 
+                    token purshace agreement, terms of use, and privacy policy.
+                <br>
+                <b>PERSALE =</b><br>
+                    pre-sale landing page creation social media pages 
+                    creation presentation making,
+                    pre-sale marketing on internet
+                <br>
+                <b>technical development =</b><br>
+                    smart contract creation consultation 
+                    by developers on setting up the technology tools 
+                    & coin management system developmentµ
+                <br>
+                <b style="color: rgb(211, 179, 1);">2022 Q4=</b><br>
+                <b>marketing =</b><br>
+                    worldwide marketing,
+                    bounty program setup to conduct marketing compaigns
+                <br>
+                <b>final stage =</b><br>
+                    start coin sale and manage token sale 
+                    & PRICE WITH A HELP SKILLED financial analyst
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
