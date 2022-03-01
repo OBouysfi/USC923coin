75420be42fe0b64c1c0c246111a4ffea2f4b416a
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:37:36 +0000

    Update buy.blade.php

---
+++ resources/views/front/buy.blade.php
@@ -0,0 +1,68 @@
+ <!--next-->
+ <section id="buy" class="py-5 text-light">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md py-4">
+                <h2>How To Buy (MetaMask)</h2>
+                <p class="lead">
+                <b style="color: rgb(211, 179, 1);">Step 1: Install Metamask</b> <br>
+                Open your browser and visit <a href="https://metamask.io/download/">metamask</a>
+                io to download the MetaMask Extension and set up a wallet.
+                You can also download the MetaMask app on iPhone & Android.
+                <br>
+                <b style="color: rgb(211, 179, 1);">Step 2: Send BNB to MetaMask</b><br>
+                Transfer BNB to your Metamask wallet from your existing wallet 
+                Be sure to use BSC network. (learn more)
+<br>
+<b style="color: rgb(211, 179, 1);">Step 3: Connect to Pancakeswap.finance</b><br>
+Make sure the network tab in your Metamask wallet is set to “Binance Smart Chain”.
+
+<br>
+<b style="color: rgb(211, 179, 1);">Step 4: Swap $BNB For $USC923</b><br>
+Follow the PancakeSwap link on our website and swap $BNB for $USC923. 
+Always confirm the offical contract address: "contract"
+<br>
+
+<b style="color: rgb(211, 179, 1);">Step 5: Tell all your friends to buy also.</b><br>
+Unofficial rules of $USC923 state that after you buy USC923 
+it’s your sacred duty to the community to spread the word of $USC923;) because we love network effects;)
+<br>
+
+<b style="color: rgb(211, 179, 1);">Step 6: Fasten your seatbelt because we are going to the moon.</b><br>
+In ordinary circumstances,
+you should never text and drive but because rockets are self-driving 
+you can sit back and spread the good word of $$USC923.
+
+                </p>
+            </div>
+            
+            <div class="col-md py-4">
+                <h2>How To Buy (Trust Wallet)</h2>
+                <p class="lead">
+<b style="color: rgb(211, 179, 1);">Step 1: Connect to a Pancakeswap.finance using your TrustWallet</b><br>
+Visit <a href="https://pancakeswap.finance/">pancakeswap.finance</a> on your mobile device.
+Connect to Pancakeswap and authenticate yourself with Trust Wallet.
+<br>
+
+<b style="color: rgb(211, 179, 1);">Step 2: Swap $BNB For $USC923 </b><br>
+Follow the PancakeSwap link on our website and swap $BNB for $USC923 .
+Always confirm the official contract address: "contract"
+<br>
+
+<b style="color: rgb(211, 179, 1);">Step 3: Tell all your friends to buy also.</b><br>
+Unofficial rules of $USC923 state that after you buy $DogeBNB
+it’s your sacred duty to the community to spread the word of $USC923 ;)
+because we love network effects;)
+<br>
+
+<b style="color: rgb(211, 179, 1);">Step 4: Fasten your seatbelt because we are going to the moon.</b><br>
+In ordinary circumstances, you should never text and 
+drive put because rockets are self-driving you can sit back and spread the good word of $USC923.
+
+
+                </p>
+            </div>
+        </div>
+    </div>
+
+</section>
