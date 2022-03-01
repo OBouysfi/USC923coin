57cfb897181f09137dd6fd80763803dbc822c854
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:38:13 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+/**Frontend */
+Route::get('', 'App\Http\Controllers\FrontController@index')->name('home');
+/**Backend */
