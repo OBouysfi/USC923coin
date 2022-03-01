ee98cb5222776815a6d789a10f0ffc752ac042b2
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:38:03 +0000

    Update tokenomic.blade.php

---
+++ resources/views/front/tokenomic.blade.php
@@ -0,0 +1,17 @@
+<section id="hero" class="bg-dark text-light text-center text-sm-start py-5">
+    <div class="container">
+        <div class="d-sm-flex align-items-center">
+            <div>
+                <h1 class="display-5">Tokenomic <span style="color: rgb(211, 179, 1);font-weight: bold;">USC923</span></h1>
+                <p class="py-1 lead">
+                   <b>Token name: $USC923</b> <br>
+                   Circulating Suppl: 100,000,000,000,000 USC923 <br>
+                   Token type: BEP-20 Binance Smart Chain <br>
+                   Tokenomics : Publicly sold :70%     Active team :10%     Marketing : 5%    Liquidity : 5%    BSC projects airdrops : 5% :Foundation 5%
+                </p>
+                <a href="" style="font-weight: bold;" class="btn btn-warning">How To Buy</a>
+            </div>
+            <img src="{{asset('front/img/rin.svg')}}" width="250" class="img-fluid d-none d-sm-block" alt="" srcset="">
+        </div>
+    </div>
+</section>
