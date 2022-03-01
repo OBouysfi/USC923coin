9811ae5fae534eac9094dbcbeb8ad5a17fbde6e9
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:37:54 +0000

    Create contract.blade.php

---
+++ resources/views/front/contract.blade.php
@@ -0,0 +1,17 @@
+<section id="contra" class="py-5 text-center">
+    <div class="container">
+        <h2 class="mb-2">Smart Contract > <span style="color: rgb(211, 179, 1);">USC923COIN</span></h2>
+        <form class="form-floating">
+            <input style="border-radius: 8px;font-weight: bold;" type="email" class="form-control bg-dark text-light lead text-center" id="floatingInputValue" value="0x00000000000000001">
+            <label for="floatingInputValue" style="color: white;">selection and copy</label>
+        </form>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <a href="https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
+            <i class="bi bi-whatsapp my-float"></i>
+        </a>
+    </div>
+</section>
+<hr>
