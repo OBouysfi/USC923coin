a56266498c20d20c46638262de15474976c5da0a
Othman Bouysfi <89135146+OBouysfi@users.noreply.github.com>
2022-03-01 20:37:41 +0000

    Update header.blade.php

---
+++ resources/views/front/comun/header.blade.php
@@ -0,0 +1,41 @@
+<section class="back">
+    <header>
+        <div class="navbar navbar-expand-md navbar-dark text-white">
+            <div class="container">
+                <a href="index.html"><img src="{{asset('front/img/logo.png')}}" width="65" alt="logo" srcset=""></a>
+                <button style="background-color: rgb(211, 179, 1);" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainmenu"><span class="navbar-toggler-icon"></span></button>
+
+                <div class="collapse navbar-collapse" id="mainmenu">
+                    <hr>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a href="#hero" class="nav-link">Tokenomic</a></li>
+                        <li class="nav-item"><a href="#contra" class="nav-link">Contract</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">About Us</a></li>
+                        <li class="nav-item"><a href="#contact" class="nav-link">Contact Us</a></li>
+                        <li class="nav-item"><a href="#buy" class="nav-link">How to buy</a></li>
+                        <li class="nav-item"><a href="#rode" class="nav-link">Roadmap</a></li>
+                        <li class="nav-item active"><a href="{{asset('front/white/USC923.pdf')}}" style="color: rgb(211, 179, 1);" class="nav-link">Whitepaper</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+        <div class="container">
+            <div class="welcome">
+                <h1>WELCOME TO <span>USC923COIN</span></h1>
+                <p>
+                    A Decentralized USC923 TOKEN that Evolved into a Vibrant Ecosystem
+                    If 2022 taught the crypto world anything,
+                    it's that community-driven tokens are in HIGH demand. 
+                    The PEOPLE want control of their token, 
+                    and strong communities of HODLers can be built that have each other's back.
+                    2022 - Year of the USC923
+                </p>
+                <div class="btns">
+                    <a class="btn btnSOLID" href="#buy">BUY NOW</a>
+                    <a class="btn btnActive" href="{{asset('front/white/USC923.pdf')}}">Whitepaper</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
